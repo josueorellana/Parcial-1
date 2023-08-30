d33a4502044b9e1735b7c8db7f0a326f928028d6
Wilbert José Girón Pérez <USSS039723@ugb.edu.sv>
2023-08-29 19:20:06 -0600

    aaaaaaaaaaa

---
+++ Adeleparcialejercicio1/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿
+namespace Adeleparcialejercicio1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblcantidad = new System.Windows.Forms.Label();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.txtcantidad = new System.Windows.Forms.TextBox();
+            this.lblimpuestos = new System.Windows.Forms.Label();
+            this.txtimpuestos = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblcantidad
+            // 
+            this.lblcantidad.AutoSize = true;
+            this.lblcantidad.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblcantidad.Location = new System.Drawing.Point(75, 80);
+            this.lblcantidad.Name = "lblcantidad";
+            this.lblcantidad.Size = new System.Drawing.Size(235, 20);
+            this.lblcantidad.TabIndex = 0;
+            this.lblcantidad.Text = "Ingresar cantidad de monto:";
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalcular.Location = new System.Drawing.Point(346, 243);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(112, 31);
+            this.btnCalcular.TabIndex = 1;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // txtcantidad
+            // 
+            this.txtcantidad.Location = new System.Drawing.Point(316, 80);
+            this.txtcantidad.Name = "txtcantidad";
+            this.txtcantidad.Size = new System.Drawing.Size(194, 20);
+            this.txtcantidad.TabIndex = 2;
+            this.txtcantidad.TextChanged += new System.EventHandler(this.txtcantidad_TextChanged);
+            // 
+            // lblimpuestos
+            // 
+            this.lblimpuestos.AutoSize = true;
+            this.lblimpuestos.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblimpuestos.Location = new System.Drawing.Point(75, 169);
+            this.lblimpuestos.Name = "lblimpuestos";
+            this.lblimpuestos.Size = new System.Drawing.Size(232, 20);
+            this.lblimpuestos.TabIndex = 3;
+            this.lblimpuestos.Text = "Total de impuestos a pagar:";
+            // 
+            // txtimpuestos
+            // 
+            this.txtimpuestos.Location = new System.Drawing.Point(313, 171);
+            this.txtimpuestos.Name = "txtimpuestos";
+            this.txtimpuestos.Size = new System.Drawing.Size(194, 20);
+            this.txtimpuestos.TabIndex = 4;
+            this.txtimpuestos.TextChanged += new System.EventHandler(this.txtimpuestos_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtimpuestos);
+            this.Controls.Add(this.lblimpuestos);
+            this.Controls.Add(this.txtcantidad);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.lblcantidad);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblcantidad;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.TextBox txtcantidad;
+        private System.Windows.Forms.Label lblimpuestos;
+        private System.Windows.Forms.TextBox txtimpuestos;
+    }
+}
+
---
+++ Adeleparcialejercicio1/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Adeleparcialejercicio1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtcantidad_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtimpuestos_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            decimal cantidad = decimal.Parse(txtcantidad.Text);
+
+            decimal impuesto = calcularImpuesto(cantidad);
+            string impuestoStr = impuesto.ToString("0.00");
+
+            txtimpuestos.Text = impuesto.ToString();
+        }
+
+        private decimal calcularImpuesto(decimal cantidad)
+        {
+
+            int rango = encontrarRango(cantidad);
+
+            decimal precio = tablaPrecios[rango];
+            decimal adicional = tablaAdicional[rango];
+            decimal impuesto = (cantidad - tablaDesde[rango]) * adicional / 1000 + precio;
+
+            return impuesto;
+        }
+
+        private int encontrarRango(decimal cantidad)
+        {
+            for (int i = 0; i < tablaDesde.Length; i++)
+            {
+                if (cantidad >= tablaDesde[i] && cantidad <= tablaHasta[i])
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+
+        private static readonly decimal[] tablaPrecios = new decimal[] { 1.5m, 1.5m, 3m, 6m, 9m, 15m, 39m, 63m, 93m, 125m, 195m, 255m, 300m, 340m, 490m };
+        private static readonly decimal[] tablaAdicional = new decimal[] { 0m, 3m, 3m, 3m, 2m, 2m, 2m, 1m, 0.8m, 0.7m, 0.6m, 0.45m, 0.4m, 0.30m, 0.18m };
+        private static readonly decimal[] tablaDesde = new decimal[] { 0.01m, 500m, 1000.01m, 2000.01m, 3000.01m, 8000.01m, 18000.01m, 30000.01m, 60000.01m, 100000.01m, 200000.01m, 300000.01m, 400000.01m, 500000.01m, 1000000.01m };
+        private static readonly decimal[] tablaHasta = new decimal[] { 500m, 1000m, 2000m, 3000m, 6000m, 18000m, 30000m, 60000m, 100000m, 200000m, 300000m, 400000m, 500000m, 1000000m, 99999999m };
+    }
+}
